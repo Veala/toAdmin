86bd9bdc3f006d5799bf1856908c0d4ef74fbcf1
Veala Home <ialaev86@gmail.com>
2016-08-24 22:36:17 +0400

    correcting A-Z, a-z and finish

--- toAdmin/lpdialog.cpp
+++ toAdmin/lpdialog.cpp
@@ -17,7 +17,7 @@ lpDialog::lpDialog(QWidget *parent, QString curL, QString curP, QSqlDatabase &da
     setFixedSize(size());
     this->setWindowIcon(QIcon(":/img/img.png"));
     messageBox.setWindowIcon(QIcon(":/img/img.png"));
-    QRegExp rx("[0-9A-Fa-f]+");
+    QRegExp rx("[0-9A-Za-z]+");
     Validator = new QRegExpValidator(rx,this);
     ui->lineEdit->setValidator(Validator);
     ui->lineEdit_2->setValidator(Validator);
